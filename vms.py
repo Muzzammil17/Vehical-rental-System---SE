748a55bcdd616f6ee6180cfa6264916e3d912334
Muzzammil17 <muzzammilsa@gmail.com>
2026-09-07 11:08:50 +0530

    v1.0

---
+++ vms.py
@@ -0,0 +1,8 @@
+# Library Management System - Version 1.0
+# Feature: issue and return books
+
+def issue_book(book_id, member_id):
+  print("Book", book_id, "issued to member", member_id)
+
+def return_book(book_id):
+  print("Book", book_id, "returned")
